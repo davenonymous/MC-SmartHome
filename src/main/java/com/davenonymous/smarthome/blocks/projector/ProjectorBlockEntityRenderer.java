e916da595bff5540cbb2ad6f3bfb0d7df4b15034
davenonymous <david.hilberath@gmx.de>
2025-10-08 20:11:22 +0200

    More projection rendering improvements

--- src/main/java/com/davenonymous/smarthome/blocks/projector/ProjectorBlockEntityRenderer.java
+++ src/main/java/com/davenonymous/smarthome/blocks/projector/ProjectorBlockEntityRenderer.java
@@ -1,7 +1,9 @@
 package com.davenonymous.smarthome.blocks.projector;
 
 import com.davenonymous.smarthome.SmartHome;
+import com.davenonymous.smarthome.blocks.base.FacingBaseBlock;
 import com.davenonymous.smarthome.data.HomeCore;
+import com.davenonymous.smarthome.lib.gui.CellData;
 import com.davenonymous.smarthome.lib.gui.widgets.Widget;
 import com.davenonymous.smarthome.networking.ClientCache;
 import com.mojang.blaze3d.systems.RenderSystem;
@@ -12,6 +14,8 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.state.properties.AttachFace;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -60,20 +64,41 @@ public class ProjectorBlockEntityRenderer implements BlockEntityRenderer<Project
 		int cardHeight = card.height();
 
 		int wantedSize = 256;
-		float scaleFactor = Math.min((float)wantedSize / cardWidth, (float)wantedSize / cardHeight);
-		guigraphics.pose().scale(scaleFactor, scaleFactor, scaleFactor);
 
-		if(cardWidth > cardHeight) {
-			int yOffset = (wantedSize - (int)(cardHeight * scaleFactor)) / 2;
-			guigraphics.pose().translate(0, yOffset, 0);
-		} else {
-			int xOffset = (wantedSize - (int)(cardWidth * scaleFactor)) / 2;
-			guigraphics.pose().translate(xOffset, 0, 0);
-		}
-		guigraphics.pose().translate(cardWidth, cardHeight + 32, 264);
+
+		var blockState = projector.getBlockState();
+		var attachFace = blockState.getValue(FacingBaseBlock.ATTACH_FACE);
+		var facing = projector.getBlockState().getValue(FacingBaseBlock.HORIZONTAL_FACING);
+		var shape = blockState.getShape(projector.getLevel(), projector.getBlockPos());
+
+
+		var box = shape.bounds().expandTowards(256f, 0, 256f);
+		guigraphics.pose().translate(box.getCenter().x, box.getCenter().y, box.getCenter().z);
+
 
 		guigraphics.pose().mulPose(Axis.XP.rotationDegrees(180));
-		guigraphics.pose().mulPose(Axis.YP.rotationDegrees(180));
+
+		Direction from = Direction.NORTH;
+		while(from != facing) {
+			guigraphics.pose().mulPose(Axis.YP.rotationDegrees(90));
+			from = from.getClockWise();
+		}
+
+
+
+		float scaleFactor = Math.min((float)wantedSize / cardWidth, (float)wantedSize / cardHeight);
+		guigraphics.pose().translate(0, -32, 0);
+		guigraphics.pose().scale(scaleFactor, scaleFactor, 1);
+		guigraphics.pose().translate(-cardWidth / 2f, -cardHeight, 96f);
+
+		if(attachFace == AttachFace.CEILING) {
+			guigraphics.pose().mulPose(Axis.XP.rotationDegrees(-90));
+			guigraphics.pose().translate(0, 0, 192);
+		} else if(attachFace == AttachFace.FLOOR) {
+			guigraphics.pose().mulPose(Axis.XP.rotationDegrees(90));
+			guigraphics.pose().translate(0, -192, 0);
+		}
+
 		long lastUpdate = widgetUpdateTimes.getOrDefault(cardId, 0L);
 		long gameTick = projector.getLevel().getGameTime();
 		boolean needsUpdate = gameTick % 100 == 0 && lastUpdate != gameTick;
